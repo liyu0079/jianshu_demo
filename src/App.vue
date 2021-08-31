237806aafdf14a2b5ac24442f228e180414dbe9b
liyu <1779371276@qq.com>
2021-08-31 21:14:37 +0800

    create

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+   <div>
+     hello jianshu
+     <el-button type="success">按钮</el-button>
+     
+   </div>
+</template>
+
+<script>
+export default {
+  created(){
+       this.$http({
+         path: 'https://cnodejs.org/api/v1/topics',
+         method: 'get',
+         params:{
+
+         }
+       }).then(res=>{
+         console.log(res)
+       })
+  }
+    // mounted(){
+    //   this.$axios.get('http://localhost:3000/users/find').then(res=>{
+    //     console.log(res.data)
+    //   }).catch(err=>{
+    //     console.error('请求异常',err)
+    //   })
+    // }
+}
+</script>
+
+<style>
+
+</style>
